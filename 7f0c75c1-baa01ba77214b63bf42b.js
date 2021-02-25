a27d21c63742cbea792de46937494dbafe7aeb3b
ahmed <ahmed.elq53@gmail.com>
2021-02-25 12:59:28 +0300

    Update

--- 7f0c75c1-baa01ba77214b63bf42b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{Iu7H:function(n,t,H){"use strict";H.d(t,"a",(function(){return V}));var e=H("Lnxd");function V(n){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M9 1H1V9H9V6H11V20H15V23H23V15H15V18H13V6H15V9H23V1H15V4H9V1ZM21 3H17V7H21V3ZM17 17H21V21H17V17Z",fill:"currentColor"}}]})(n)}}}]);
-//# sourceMappingURL=7f0c75c1-baa01ba77214b63bf42b.js.map
--- de71a805-8de36386e467fecb1e0a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{rCLJ:function(t,n,c){"use strict";c.d(n,"a",(function(){return i}));var a=c("Lnxd");function i(t){return Object(a.a)({tag:"svg",attr:{version:"1.2",baseProfile:"tiny",viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M18.414 10.656c-.781-.781-2.047-.781-2.828 0l-1.586 1.586v-7.242c0-1.105-.896-2-2-2-1.105 0-2 .895-2 2v7.242l-1.586-1.586c-.781-.781-2.047-.781-2.828 0s-.781 2.047 0 2.828l6.414 6.414 6.414-6.414c.781-.781.781-2.046 0-2.828z"}}]})(t)}}}]);
-//# sourceMappingURL=de71a805-8de36386e467fecb1e0a.js.map
